f8338e1e75a8141ff2e6df7ea15723f31b55c254
Oliver Gingerich <Ogingerich5354@conestogac.on.ca>
2025-03-12 14:36:51 -0400

    Initial commit
    Added base files. Added navbar that sends user back to main page.

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Navbar from './Navbars/Navbar'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Navbar/>
+    {/*TITLE*/}
+    {/*HOW TO PLAY*/}
+    {/*STORY*/}
+    {/*GAME*/}
+    {/*VERSION NOTES*/}
+    </>
+  )
+}
+
+export default App
---
+++ src/Navbars/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { useState } from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+        <nav className="bg-fuchsia-950 p-4">
+            <div className="container mx-0 flex justify-between items-center">
+                {/* Brand Name */}
+                <a href="https://oliver-gi.github.io" className="text-white text-lg font-semibold">Oliver
+                </a>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
