f0aa8fb5f9b5fdfa454ee98892763da45a39e2fb
Oliver Gingerich <Ogingerich5354@conestogac.on.ca>
2025-03-18 17:05:29 -0400

    Added background image to game div

--- src/Static/game.jsx
+++ src/Static/game.jsx
@@ -3,9 +3,7 @@ import React from 'react'
 
 const Game = () => {
     return(
-        <div class="bg-fuchsia-950 h-[40vh] w-[100vw]">
-            <h1 class="text-2xl mt-5 text-center font-DP text-white">
-                This will hold the the game</h1>
+        <div class="bg-[url('../Resources/Backgrounds/startScreen.png')] bg-no-repeat bg-contain bg-center w-[100vw] h-[80vh]">
         </div>
     )
 }
--- src/Static/versionNotes.jsx
+++ src/Static/versionNotes.jsx
@@ -18,8 +18,8 @@ const Version = () =>{
             open && <div class='w-[90vw] mb-[5vh] mt-[0px] ml-[5vw] mt-[0px] bg-fuchsia-950/75 text-[#e3c2fc]'>
                     <div>
                         <div class="ml-[1rem] pb-[1vh]"> 
-                        <strong>17/03/2025</strong><br/><br/>
-                        Added functional instruction and story tabs that open on click/tap. Converted version notes section to an openable tab.
+                        <strong>18/03/2025</strong><br/><br/>
+                        Added background image to the game screen. Need to figure out how I will change it on button click.
                         </div>
                     </div>
             </div>}
