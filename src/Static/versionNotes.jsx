9945989e616cd54b6eb8b1bcc2a4acdbaafd25bf
Oliver Gingerich <Ogingerich5354@conestogac.on.ca>
2025-06-24 12:51:32 -0400

    Fixed date in version notes.

--- src/Static/versionNotes.jsx
+++ src/Static/versionNotes.jsx
@@ -23,7 +23,7 @@ const Version = () =>{
             open && <div class='w-[90vw] mb-[5vh] mt-[0px] ml-[5vw] mt-[0px] bg-fuchsia-950/75 text-[#e3c2fc]'>
                     <div>
                         <div class="ml-[1rem] pb-[1vh]"> 
-                        <strong>12/06/2025</strong><br/><br/>
+                        <strong>12/24/2025</strong><br/><br/>
                         - Designed character description screen.<br/>
                         - Buttons will redirect to information screens about their relative character in future.<br/>
                         - Back button moves you back to difficulty screen.<br/>
